e737b45fe44976a0cdb613690783ad76dec12416
Furqan Munir <furqanmunir.techtimize@gmail.com>
2025-06-03 16:16:19 +0500

    layouts, sidebar, and homepage made responsive

--- src/layouts/main/components/Topbar.tsx
+++ src/layouts/main/components/Topbar.tsx
@@ -1,26 +1,39 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { FiBell, FiSettings, FiUser, FiMenu } from 'react-icons/fi'
 import type { ITopBarProps } from '../types'
 import { useAppSelector } from '@/hooks/storeHooks'
 
-const TopBar = ({ toggleSidebar }: ITopBarProps) => {
+const TopBar = ({ toggleSidebar, layout }: ITopBarProps) => {
+  const [showMobileMenu, setShowMobileMenu] = useState(layout === 'mobile')
   const auth = useAppSelector((state) => state.auth)
 
   return (
-    <div className='w-full flex items-center justify-between px-6 py-4 shadow-sm bg-primary-3'>
-      <div className='w-52 flex items-center gap-4 z-50'>
-        <FiMenu
-          className='w-6 h-6 text-gray-300 cursor-pointer hover:text-gray-800'
-          onClick={toggleSidebar}
-        />
-        <h1 className='text-xl font-bold text-gray-300'>Logo</h1>
+    <div className='w-full flex flex-col md:flex-row items-center justify-between px-4 md:px-6 py-4 shadow-sm bg-primary-3'>
+      <div className='w-full md:w-52 flex items-center justify-between md:justify-start gap-4 z-50'>
+        <div className='flex items-center gap-4'>
+          <FiMenu
+            className='w-6 h-6 text-gray-300 cursor-pointer hover:text-gray-800'
+            onClick={toggleSidebar}
+          />
+          <h1 className='text-xl font-bold text-gray-300'>Logo</h1>
+        </div>
+        <button
+          className='md:hidden text-gray-300'
+          onClick={() => setShowMobileMenu(!showMobileMenu)}
+        >
+          <FiMenu className='w-6 h-6' />
+        </button>
       </div>
 
-      <div className='flex-1 flex justify-center'>
+      <div className='hidden md:flex flex-1 justify-center'>
         <span className='text-white text-lg'>PEI Management Platform</span>
       </div>
 
-      <div className='w-52 flex items-center justify-end space-x-6'>
+      <div
+        className={`${
+          showMobileMenu ? 'flex' : 'hidden'
+        } md:flex w-full md:w-52 flex-col md:flex-row items-center justify-center md:justify-end space-y-4 md:space-y-0 md:space-x-6 mt-4 md:mt-0`}
+      >
         <FiBell className='w-6 h-6 text-gray-300 cursor-pointer hover:text-gray-800' />
         <FiSettings className='w-6 h-6 text-gray-300 cursor-pointer hover:text-gray-800' />
         <div className='flex items-center cursor-pointer'>
--- src/layouts/main/types.ts
+++ src/layouts/main/types.ts
@@ -2,6 +2,7 @@ import type { ModuleTypeEnum } from '@/store/reducers/moduleSlice'
 
 export interface ITopBarProps {
   toggleSidebar: () => void
+  layout: 'mobile' | 'desktop'
 }
 
 export interface ISidebarProps {
