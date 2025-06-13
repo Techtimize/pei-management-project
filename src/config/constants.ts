9545ddd738fde75d62949c15677b8883a23ba05d
Furqan Munir <furqanmunir.techtimize@gmail.com>
2025-06-13 14:47:41 +0500

    dropdown values changed for homepage, search list data added and being able to search for selected attribute

--- src/config/constants.ts
+++ src/config/constants.ts
@@ -4,8 +4,19 @@ export interface ISearchByOption {
 }
 
 export const searchByOptions = [
-  { value: 'name', label: 'Name' },
-  { value: 'duns', label: 'DUNS' }
+  // PEI & Portfolio Options
+  { value: 'pb_name', label: 'Pitchbook Name' },
+  { value: 'duns_number', label: 'DUNS Number' },
+  { value: 'swift_client_number', label: 'Swift Client Number' },
+  { value: 'pb_id', label: 'Pitchbook ID' },
+  { value: 'gmdm_id', label: 'GMDM ID' },
+  { value: 'dgmf_id', label: 'DGMF ID' }
+
+  // Portfolio Options
+  // { value: 'name', label: 'Name' },
+  // { value: 'duns', label: 'DUNS' },
+  // { value: 'name', label: 'Name' },
+  // { value: 'duns', label: 'DUNS' },
 ]
 
 export interface ICompany extends ISearchByOption {
